ad4341d9260e84f1104f3dbcabd0d7015e891149
developer <noreply@null.org>
2025-04-19 13:48:31 +0800

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,6 @@ replace_env_vars() {
   # Replace the password placeholder in all HTML files
   find /usr/share/nginx/html -type f -name "*.html" -exec sed -i "s/window.__ENV__.PASSWORD = \"{{PASSWORD}}\";/window.__ENV__.PASSWORD = \"${PASSWORD:-}\";/g" {} \;
   
-  # Add proxy API environment variable if needed
-  if [ -n "$PROXY_API" ]; then
-    find /usr/share/nginx/html -type f -name "*.html" -exec sed -i "s/window.__ENV__.PROXY_API = false;/window.__ENV__.PROXY_API = true;/g" {} \;
-  fi
-  
   echo "Environment variables have been injected into HTML files."
 }
 
