7a0e20177808cef0b87d46016dd18f58245f51fd
旋律已经死了。 <89735151+JohnsonRan@users.noreply.github.com>
2025-06-11 23:39:39 +0800

    fix: can't play from history when deployed in docker (#617)

--- server.mjs
+++ server.mjs
@@ -40,7 +40,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   next();
 });
